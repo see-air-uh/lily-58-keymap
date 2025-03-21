f7e4bf3d6e66a1bc4e4b7b06f24e06533bf1f009
Luca Sciarra <seeairuh@macbookpro.home>
2025-03-21 12:11:12 -0400

    Adjust arrow layer, increase timeout

--- keymap.c
+++ keymap.c
@@ -19,7 +19,7 @@ uint32_t anim_sleep = 0;
 uint8_t current_idle_frame = 0;
 uint8_t current_tap_frame = 0;
 
-static long int oled_timeout = 60000;//0; // 10 minutes
+static long int oled_timeout = 600000;//0; // 10 minutes
 
 extern uint8_t is_master;
 
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                           KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
-        KC_LSFT, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                     KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_BSLS,
+        KC_LSFT, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                     KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,KC_NO, KC_BSLS,
         KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,       KC_TRNS, KC_PLUS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
             KC_TRNS, KC_TRNS, MO(3), KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
     [3] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
